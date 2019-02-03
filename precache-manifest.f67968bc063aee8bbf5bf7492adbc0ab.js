30792553a97a80fc78e2b07b5b4b1a334877c79b
Đào Ngọc Thành <daongocthanh98hy@gmail.com>
2019-02-03 14:44:02 +0700

    Updates

---
+++ precache-manifest.f67968bc063aee8bbf5bf7492adbc0ab.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c952a9a31fc77dbf4b8",
+    "url": "/sourcecodeT/portfolio-react/static/css/main.5631db85.chunk.css"
+  },
+  {
+    "revision": "8c952a9a31fc77dbf4b8",
+    "url": "/sourcecodeT/portfolio-react/static/js/main.8c952a9a.chunk.js"
+  },
+  {
+    "revision": "b67a47bcd858e19b92f2",
+    "url": "/sourcecodeT/portfolio-react/static/js/1.b67a47bc.chunk.js"
+  },
+  {
+    "revision": "57d536510b41fda6876c",
+    "url": "/sourcecodeT/portfolio-react/static/js/runtime~main.57d53651.js"
+  },
+  {
+    "revision": "06f86b26bacc768a6eff973a0d461aef",
+    "url": "/sourcecodeT/portfolio-react/static/media/profile_picture.06f86b26.jpg"
+  },
+  {
+    "revision": "6e3544b9d48004fb0a5778cfcea416d8",
+    "url": "/sourcecodeT/portfolio-react/static/media/f_logo.6e3544b9.png"
+  },
+  {
+    "revision": "0da6a83febaa081767226050dc202a11",
+    "url": "/sourcecodeT/portfolio-react/static/media/f_marker.0da6a83f.png"
+  },
+  {
+    "revision": "c7e5861b80822efe6b02976a113ddd4c",
+    "url": "/sourcecodeT/portfolio-react/static/media/landing-vibix-scheduler.c7e5861b.png"
+  },
+  {
+    "revision": "39be89b21bbaa8b11daeef61cd03856a",
+    "url": "/sourcecodeT/portfolio-react/static/media/landing-vibix-consignment.39be89b2.png"
+  },
+  {
+    "revision": "7289e5efc7250bf2d60a79b08e2624e1",
+    "url": "/sourcecodeT/portfolio-react/static/media/landing-vibix-web.7289e5ef.png"
+  },
+  {
+    "revision": "a4835157d99892077980d796152bd387",
+    "url": "/sourcecodeT/portfolio-react/static/media/landing_ifunny.a4835157.png"
+  },
+  {
+    "revision": "60a30948416e82a4a198db14a3a16224",
+    "url": "/sourcecodeT/portfolio-react/static/media/landing-quiltback.60a30948.png"
+  },
+  {
+    "revision": "e4267d6236d87136b31818d6c79ad376",
+    "url": "/sourcecodeT/portfolio-react/static/media/landing-deliwin.e4267d62.png"
+  },
+  {
+    "revision": "1d345d47fd52ae942947f939b876d709",
+    "url": "/sourcecodeT/portfolio-react/static/media/game-of-life.1d345d47.png"
+  },
+  {
+    "revision": "75418e0296630492d96c646fa381e241",
+    "url": "/sourcecodeT/portfolio-react/static/media/ha-landing.75418e02.png"
+  },
+  {
+    "revision": "7e3d2a92473b14dea460b9b87b4639bf",
+    "url": "/sourcecodeT/portfolio-react/static/media/abel-regular.7e3d2a92.ttf"
+  },
+  {
+    "revision": "32e66b5b650461435d04fb49cdd092f8",
+    "url": "/sourcecodeT/portfolio-react/static/media/timeburnerbold.32e66b5b.ttf"
+  },
+  {
+    "revision": "53c45bc3d1786fd79f705b667a74fec5",
+    "url": "/sourcecodeT/portfolio-react/static/media/timeburnernormal.53c45bc3.ttf"
+  },
+  {
+    "revision": "e4831b5b0271b2d1fdc75859713fcda1",
+    "url": "/sourcecodeT/portfolio-react/index.html"
+  }
+];
